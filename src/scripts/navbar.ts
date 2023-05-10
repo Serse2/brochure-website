10caca697da831e73858e5818567036d0d416159
sergio <sergio.berga@hotmail.it>
2023-05-10 09:30:08 +0200

    add prettier

--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -1,10 +1,9 @@
-
-const burger = document.querySelector('.burger-menu');
-const navbar = document.querySelector('.navbar');
+const burger = document.querySelector('.burger-menu')
+const navbar = document.querySelector('.navbar')
 
 if (burger && navbar) {
     burger.addEventListener('click', () => {
-        burger.classList.toggle('active');
-        navbar.classList.toggle('active');
+        burger.classList.toggle('active')
+        navbar.classList.toggle('active')
     })
 }
--- src/scripts/select-form.ts
+++ src/scripts/select-form.ts
@@ -1,25 +1,31 @@
-const formPrivato = document.querySelector('#form-privato');
-const formAzienda = document.querySelector('#form-azienda');
+const formPrivato = document.querySelector('#form-privato')
+const formAzienda = document.querySelector('#form-azienda')
 
-const radioPrivato = document.querySelector('#radio-privato');
-const radioAzienda = document.querySelector('#radio-azienda');
+const radioPrivato = document.querySelector('#radio-privato')
+const radioAzienda = document.querySelector('#radio-azienda')
 
-const labelPrivato = document.querySelector('.label-radio-privato');
-const labelAzienda = document.querySelector('.label-radio-azienda');
+const labelPrivato = document.querySelector('.label-radio-privato')
+const labelAzienda = document.querySelector('.label-radio-azienda')
 
-
-if (radioPrivato && radioAzienda && formPrivato && formAzienda && labelPrivato && labelAzienda) {
+if (
+    radioPrivato &&
+    radioAzienda &&
+    formPrivato &&
+    formAzienda &&
+    labelPrivato &&
+    labelAzienda
+) {
     radioPrivato.addEventListener('click', () => {
-        formAzienda.classList.remove('selected');
-        labelAzienda.classList.remove('selected');
-        formPrivato.classList.add('selected');
-        labelPrivato.classList.add('selected');
-    });
+        formAzienda.classList.remove('selected')
+        labelAzienda.classList.remove('selected')
+        formPrivato.classList.add('selected')
+        labelPrivato.classList.add('selected')
+    })
 
     radioAzienda.addEventListener('click', () => {
-        formAzienda.classList.add('selected');
-        formPrivato.classList.remove('selected');
-        labelAzienda.classList.add('selected');
-        labelPrivato.classList.remove('selected');
-    });
+        formAzienda.classList.add('selected')
+        formPrivato.classList.remove('selected')
+        labelAzienda.classList.add('selected')
+        labelPrivato.classList.remove('selected')
+    })
 }
